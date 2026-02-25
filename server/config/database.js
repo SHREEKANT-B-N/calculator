5f891d50d0c399eea711a6274ecaaed6fd63f64b
shreekant--1 <bnshreekant@gmail.com>
2026-02-25 14:52:18 +0530

    Create MongoDB connection file

---
+++ server/config/database.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        const uri = process.env.MONGODB_URI;
+        await mongoose.connect(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('MongoDB connected successfully');
+    } catch (error) {
+        console.error('MongoDB connection failed:', error.message);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
